edb8bdca748d7cf7dec74fec2c33305bb3475acd
CaroKris <ckhilpisch@gmail.com>
2021-03-19 14:32:10 -0500

    adding models of user and blog to the models folder, along with an index file to route the folders

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,4 @@
 const withAuth = (req, res, next) => {
-    // if you're not logged in, you are redirected to the login page
     if (!req.session.logged_in) {
       res.redirect('/login');
     } else {
