fc028b5ec9e36304d076a5bb5f96b60e88735219
CaroKris <ckhilpisch@gmail.com>
2021-03-19 23:08:35 -0500

    adding api routes

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Blog } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const newBlog = await Blog.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlog);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: 'That blog does not live here!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
