caccf24430d5232f03f4ebcb71af84786cada4d5
CaroKris <ckhilpisch@gmail.com>
2021-03-25 15:03:26 -0500

    separating the signup and the login pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -64,10 +64,17 @@ router.get("/dashboard", withAuth, async (req, res) => {
 });
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect("/homepage");
+    res.redirect("/dashboard");
     return;
   }
   res.render("login");
 });
 
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+  res.render("signup");
+});
 module.exports = router;
