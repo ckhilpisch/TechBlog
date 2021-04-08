3e44b2f015043c0c3ecc5a0aae476ace3c61e7f1
CaroKris <ckhilpisch@gmail.com>
2021-04-07 23:48:09 -0500

    README

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
-const Sequelize = require("sequelize");
-require("dotenv").config("");
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
 let sequelize;
 
@@ -9,13 +9,14 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PASS,
     {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
     }
   );
 }
 
 module.exports = sequelize;
+
